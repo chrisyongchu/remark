e1461e91a9eb43200611eed69bbdcab3e4c61b87
Chris Yongchu <yongchuc@yahoo.com>
2019-04-02 16:04:29 -0700

    Update remark.js
    Fixed input[type="text"] clearing existing classes if tabbable is enabled/disabled.

--- remark.js
+++ remark.js
@@ -2,7 +2,7 @@
  * ----------------------------------------------------------------------------
  * Name: Remark Theme for HighQ Publisher 5.x
  * Author: Christopher Yongchu, christopher.yongchu@highq.com
- * Theme Version: 1.14
+ * Theme Version: 1.16
  * Last Update: 02/04/2019
  */
 
@@ -30,6 +30,29 @@ window.themes.fn = {
 ;(function (themes, $js) {
   themes = themes || {};
   var themeKey = 'data-remark-theme';
+  var Attribute$1 = {
+    ID: 'id'
+  };
+  var ClassName$1 = {
+    BOXED_LAYOUT: 'remark-layout-boxed',
+    PRETTYPANELS: 'remark-pretty-panels',
+    SEARCHABLE: 'searchable'
+  };
+  var Element$1 = {
+    COMPONENTS: '[id^="componentRender"]',
+    CONTACT_COMP: '.contact_comp',
+    FORM_GROUPS: '.form-group:not(:first)',
+    ROW: '.row',
+    ROW_COLUMN: '.setRowColClass'
+  };
+  var Template$1 = {
+    PEOPLE_INPUT: '<div class="row">' +
+                    '<div class="col-xs-12">' +
+                      '<input type="text" class="filterPeopleList form-control margBott20">' +
+                    '</div>' +
+                  '</div>'
+  };
+
   
   var Remark = function (wrapper, options) {
     return this.init(wrapper, options)
@@ -61,10 +84,10 @@ window.themes.fn = {
         wrapper.attr(themeKey, this.options.theme);
       }
       if (this.options.panels) {
-        wrapper.addClass('remark-pretty-panels');
+        wrapper.addClass(ClassName$1.PRETTYPANELS);
       }
       if (this.options.boxed) {
-        wrapper.addClass('remark-layout-boxed');
+        wrapper.addClass(ClassName$1.BOXED_LAYOUT);
       }
 
       return this;
@@ -75,15 +98,9 @@ window.themes.fn = {
         $js.ajax({
           context: document.body
         }).done(function () {
-          wrapper.find('.contact_comp').each(function () {
-            $js(this).closest('[id^="componentRender"]').addClass('searchable');
-            $js(this).children().first().after(
-              '<div class="row">' +
-                '<div class="col-xs-12">' +
-                  '<input type="text" class="filterPeopleList form-control margBott20">' +
-                '</div>' +
-              '</div>'
-            );
+          wrapper.find(Element$1.CONTACT_COMP).each(function () {
+            $js(this).closest(Element$1.COMPONENTS).addClass(ClassName$1.SEARCHABLE);
+            $js(this).children().first().after(Template$1.PEOPLE_INPUT);
           });
         });
       }
@@ -95,8 +112,8 @@ window.themes.fn = {
         $js.ajax({
           context: document.body
         }).done(function () {
-          $js('.setRowColClass').find('.form-group:not(:first)').each(function () {
-            var _row = $js(this).closest('.row').attr('id');
+          $js(Element$1.ROW_COLUMN).find(Element$1.FORM_GROUPS).each(function () {
+            var _row = $js(this).closest(Element$1.ROW).attr(Attribute$1.ID);
             $js(this).children().first().append(
               '<div class="tabbable-container">Tabbable: ' +
                 '<button class="toggle-tabbable" for="' + _row + '"><i class="fa fa-toggle-off"></i></button>' +
@@ -120,15 +137,115 @@ window.themes.fn = {
 
 $js(function () {
 
+  var AUTHOR = "Christopher Yongchu";
+  var COMPANY = "HighQ Solutions, Ltd.";
+  var NAME = "Remark for HighQ Publisher 5.x";
+  var ESCAPE_KEYCODE = 27;
+  var Attribute = {
+    DATA_REMARK_KEY: "data-remark",
+    ID: "id",
+    PANEL: "panel_",
+    NAV_TABS: "nav-tabs",
+    TAB: "tabpanel",
+    TAB_LIST: "tablist",
+    ROLE: "role"
+  };
+  var ClassName = {
+    ACTIVE: "active",
+    FADE: "tab-pane fade",
+    PANEL: "tab-content",
+    HIDE: "hide",
+    SHOW: "active in",
+    TOGGLE_ON: 'fa-toggle-on'
+  };
   var Element = {
+    DATA_TAB_KEY: "[data-remark='nav-tabs']",
+    CONTAINER: "<div class='tab-content'></div>",
+    FORMGROUP: '.form-group',
+    MAINTITLE: ".MainTitle",
+    NAV_TABS: "<ul class='nav nav-tabs' data-remark='nav-tabs'></ul>",
     PEOPLE: '.thumbOuter',
     PEOPLELIST: '.filterPeopleList',
     ROWCLASSICON: '.rowColClassico',
-    SEARCHABLE: '.searchable'
+    REMARK: ".remark-tabs",
+    REMARK_PANEL: ".remark-tabs .sortable-list",
+    ROW: ".row",
+    SEARCHABLE: '.searchable',
+    TOGGLE_TABS: '.toggle-tabbable',
+    TABBABLE_ICON: '.toggle-tabbable i',
+    TEXTINPUT: 'input[type="text"]',
+    TITLE: ".Titletxt"
   };
   var Event = {
     CLICK: 'click',
     KEYUP: 'keyup'
+  };
+  var Selector = {
+    CONTAINER: ".tab-content",
+    PANEL: ".tab-content div",
+    TITLE: ".Titletxt"
+  };
+  var Template = {
+    NAV_TABS: "<ul class='nav nav-tabs' data-remark='nav-tabs'></ul>"
+  };
+
+  function createTabs(Titletxt, index, _column, _row) {
+    var _tabs = $js(Element.DATA_TAB_KEY);
+    var _cID = $js(_column).attr(Attribute.ID);
+    var _columnObj = "#" + $js(_column).attr(Attribute.ID);
+    var _rID = $js(_row).attr(Attribute.ID);
+    var _rowObj = "#" + $js(_row).attr(Attribute.ID);
+
+    if (_row) {
+     $js(_rowObj).each(function () {
+       var _this = $js(this);
+       _this.find(_columnObj).each(function () {
+         $js(this).find(_tabs).append(
+           '<li class="nav-item">' +
+             '<a class="nav-link" id="' + _rID + _cID + 'tab' + index + '" data-toggle="tab" role="tab" href="#panel_' + _rID + _cID + index + '">' + $js(Titletxt).html() + '</a>' +
+           '</li>'
+          );
+       });
+     });
+    } else {
+     _tabs.append(
+      '<li class="nav-item">' +
+        '<a class="nav-link" id="tab_' + index + '" data-toggle="tab" role="tab" href="#panel_' + index + '">' + $js(Titletxt).html() + '</a>' +
+      '</li>'
+     );
+    }
+    _tabs.each(function () {
+     $js(this).children().first().addClass(ClassName.ACTIVE);
+    });
+  }
+
+  function createPanels(obj, colID) {
+    var _first = obj.find(Selector.CONTAINER).children().first();
+    if (colID) {
+      obj.find(Selector.CONTAINER).children().each(function (index) {
+        var _this = $js(this);
+         var _parentRow = _this.closest(Element.ROW).attr(Attribute.ID);
+         var _panelColumn = $js(this).closest(Element.REMARK).attr(Attribute.ID);
+         _this.attr(Attribute.ID, Attribute.PANEL + _parentRow + _panelColumn + index);
+         _this.attr(Attribute.ROLE, Attribute.TAB).addClass(ClassName.FADE);
+         _first.addClass(ClassName.SHOW);
+       });
+       
+       $js(Element.REMARK).each(function () {
+         $js(this).find(Selector.CONTAINER).children().first().addClass(ClassName.SHOW);
+       })
+
+    } else {
+       obj.find(Selector.CONTAINER).children().each(function (index) {
+         var _this = $js(this);
+         _this.attr(Attribute.ID, Attribute.PANEL + index);
+         _this.attr(Attribute.ROLE, Attribute.TAB).addClass(ClassName.FADE);
+       });
+
+      $js(Element.DATA_TAB_KEY).each(function () {
+        $js(this).next().children().first().addClass(ClassName.SHOW);
+      });
+    }
   }
 
   // Case insensitive contains selector
@@ -151,17 +268,43 @@ $js(function () {
   });
 
   $js(document).on(Event.CLICK, Element.ROWCLASSICON, function () {
-    $js(this).closest('li').find('input[type="text"]').each(function () {
-      var _icons = $js(this).parent().prev().find('.toggle-tabbable i');
-      ($js(this).val() === 'remark-tabs') ? _icons.addClass('fa-toggle-on') : _icons.removeClass('fa-toggle-on');
+    $js(this).closest('li').find(Element.TEXTINPUT).each(function () {
+      var _icons = $js(this).parent().prev().find(Element.TABBABLE_ICON);
+      ($js(this).val().includes('remark-tabs')) ? 
+        (_icons.addClass(ClassName.TOGGLE_ON), $js(this).attr('disabled', true)) :
+        (_icons.removeClass(ClassName.TOGGLE_ON), $js(this).attr('disabled', false));
     });
   });
 
-  $js(document).on(Event.CLICK, '.toggle-tabbable', function (e) {
+  $js(document).on(Event.CLICK, Element.TOGGLE_TABS, function (e) {
     e.preventDefault();
-    var _input = $js(this).closest('.form-group').find('input[type="text"]');
-    $js(this).children().toggleClass('fa-toggle-on');
-    (_input.val() === 'remark-tabs') ? _input.val('') : _input.val('remark-tabs');
+    var _input = $js(this).closest(Element.FORMGROUP).find(Element.TEXTINPUT);
+    $js(this).children().toggleClass(ClassName.TOGGLE_ON);
+    (_input.val().includes('remark-tabs')) ? 
+      (_input.val(function (index, value) { return _input.val().replace('remark-tabs', '') }), _input.attr('disabled', false)) : 
+      (_input.val(function (index, value) { return value + ' remark-tabs' }), _input.attr('disabled', true));   
   });
+
+  if ($js(Element.REMARK).length) {
+    $js(Element.REMARK_PANEL).addClass(ClassName.PANEL);
+    $js(Element.REMARK_PANEL).before(Template.NAV_TABS);
+    // using $.ajax() to construct tabs and panels after ajax content is done loading.
+    $js.ajax({
+      context: document.body
+    }).done(function () {
+      var _element = $js(Element.REMARK);
+      $js(Element.REMARK_PANEL).find(Element.TITLE).each(function (index) {
+        var _column = $js(this).closest(Element.REMARK);
+        var _row = $js(this).closest(Element.ROW);
+
+        createTabs(this, index, _column, _row);
+      });
+      var colID = $js(Element.REMARK).each(function () {
+        $js(this).parent(Element.COLUMN).attr(Attribute.ID);
+      });
+      
+      createPanels(_element, colID);
+    });
+  }
 
 });
